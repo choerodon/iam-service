e3eac8250eedabe58e1b2263d3a6c71ffdf5494a
flyleft <jcalaz@163.com>
2018-06-08 12:09:58 +0800

    [IMP] 模糊搜索时多个参数则搜索不到数据

--- src/main/java/io/choerodon/iam/IAMServiceApplication.java
+++ src/main/java/io/choerodon/iam/IAMServiceApplication.java
@@ -18,7 +18,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @EnableFeignClients("io.choerodon")
 @EnableEurekaClient
 @SpringBootApplication
-@EnableChoerodonResourceServer
+//@EnableChoerodonResourceServer
 @EnableAsync
 public class IAMServiceApplication {
 
