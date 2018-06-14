afd9d670ee1ad15f22825d2493429dbfe5e22ea4
bohenmian <bohenmian@gmail.com>
2018-06-14 20:33:43 +0800

    [IMP] 修改上传头像的返回为相对地址

--- src/main/java/io/choerodon/iam/infra/feign/fallback/FileFeignClientFallback.java
+++ src/main/java/io/choerodon/iam/infra/feign/fallback/FileFeignClientFallback.java
@@ -1,7 +1,9 @@
 package io.choerodon.iam.infra.feign.fallback;
 
 import io.choerodon.core.exception.CommonException;
+import io.choerodon.iam.api.dto.FileDTO;
 import io.choerodon.iam.infra.feign.FileFeignClient;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,6 +16,11 @@ public class FileFeignClientFallback implements FileFeignClient {
 
     @Override
     public ResponseEntity<String> uploadFile(Long organizationId, String backetName, String fileName, MultipartFile multipartFile) {
+        throw new CommonException("error.file.upload");
+    }
+
+    @Override
+    public ResponseEntity<FileDTO> upload(String bucketName, String fileName, MultipartFile multipartFile) {
         throw new CommonException("error.file.upload");
     }
 }
