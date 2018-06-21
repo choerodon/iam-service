8e85ec9ef73f923f6598af8e1c74e4d5f9214a9c
flyleft <jcalaz@163.com>
2018-06-21 13:57:10 +0800

    [IMP] 修改菜单和权限的关联通过code关联，而不是通过id

--- src/main/resources/script/db/iam_menu_permission.groovy
+++ src/main/resources/script/db/iam_menu_permission.groovy
@@ -11,4 +11,16 @@ databaseChangeLog(logicalFilePath: 'script/db/iam_menu_permission.groovy') {
         }
         addUniqueConstraint(tableName: 'iam_menu_permission', columnNames: 'menu_id, permission_id')
     }
+
+    changeSet(author: 'jcalaz@163.com', id: '2018-06-21-iam-menu-permission-code') {
+        dropTable(tableName: 'iam_menu_permission')
+        createTable(tableName: "iam_menu_permission") {
+            column(name: 'id', type: 'BIGINT UNSIGNED', autoIncrement: true, remarks: '表ID，主键，供其他表做外键，unsigned bigint、单表时自增、步长为 1') {
+                constraints(primaryKey: true)
+            }
+            column(name: 'menu_id', type: 'BIGINT UNSIGNED', remarks: '菜单id')
+            column(name: 'permission_code', type: 'VARCHAR(128)', remarks: '权限的标识')
+        }
+        addUniqueConstraint(tableName: 'iam_menu_permission', columnNames: 'menu_id, permission_code')
+    }
 }
