cb45da203e6c85626eed9b6629a51d0e81482ffa
superlee <superleader8@gmail.com>
2018-07-16 15:10:52 +0800

    [IMP] client校验优化

--- src/main/java/io/choerodon/iam/api/validator/ClientValidator.java
+++ src/main/java/io/choerodon/iam/api/validator/ClientValidator.java
@@ -1,12 +1,13 @@
 package io.choerodon.iam.api.validator;
 
-import io.choerodon.core.convertor.ConvertHelper;
 import io.choerodon.core.exception.CommonException;
 import io.choerodon.iam.api.dto.ClientDTO;
 import io.choerodon.iam.infra.common.utils.JsonUtils;
 import io.choerodon.iam.infra.dataobject.ClientDO;
 import io.choerodon.iam.infra.mapper.ClientMapper;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 /**
  * @author wuguokai
@@ -41,36 +42,30 @@ public class ClientValidator {
      * @return 校验之后的客户端对象
      */
     public ClientDTO update(Long orgId, Long clientId, ClientDTO clientDTO) {
-        ClientDO clientDO = clientMapper.selectByPrimaryKey(clientId);
-        if (clientDO == null) {
-            throw new CommonException("error.client.not.exist");
-        }
-        if (!orgId.equals(clientDO.getOrganizationId())) {
-            throw new CommonException("error.organizationId.not.same");
-        }
-        if (clientDTO.getName() == null) {
-            throw new CommonException("error.clientName.null");
-        }
-        if (!clientDO.getName().equals(clientDTO.getName())) {
-            clientDO = new ClientDO();
-            clientDO.setName(clientDTO.getName());
-            if (!clientMapper.select(clientDO).isEmpty()) {
-                throw new CommonException("error.clientName.exist");
+        String name = Optional.ofNullable(clientDTO.getName()).orElseThrow(() -> new CommonException("error.clientName.null"));
+        Optional.ofNullable(clientMapper.selectByPrimaryKey(clientId))
+                .map(c -> {
+                    Long organizationId = c.getOrganizationId();
+                    if (!orgId.equals(organizationId)) {
+                        throw new CommonException("error.organizationId.not.same");
+                    }
+                    return c;
+                })
+                .orElseThrow(() -> new CommonException("error.client.not.exist"));
+        ClientDO client = new ClientDO();
+        client.setName(name);
+        Optional.ofNullable(clientMapper.selectOne(client))
+                .orElseThrow(() -> new CommonException("error.clientName.exist"));
+        if (clientDTO.getAdditionalInformation() == null) {
+            clientDTO.setAdditionalInformation("{}");
+        } else {
+            //校验json格式
+            if (!JsonUtils.isJSONValid(clientDTO.getAdditionalInformation())) {
+                throw new CommonException("error.client.additionalInfo.notJson");
             }
         }
-        clientDO.setSecret(clientDTO.getSecret());
-        clientDO.setAuthorizedGrantTypes(clientDTO.getAuthorizedGrantTypes());
-        clientDO.setAccessTokenValidity(clientDTO.getAccessTokenValidity());
-        clientDO.setRefreshTokenValidity(clientDTO.getRefreshTokenValidity());
-        clientDO.setWebServerRedirectUri(clientDTO.getWebServerRedirectUri());
-        if (clientDTO.getAdditionalInformation() != null && !JsonUtils.isJSONValid(clientDTO.getAdditionalInformation())) {
-            throw new CommonException("error.client.additionalInfo.notJson");
-        }
-        clientDO.setAdditionalInformation(clientDTO.getAdditionalInformation());
-        if (clientDO.getAdditionalInformation() == null) {
-            clientDO.setAdditionalInformation("{}");
-        }
-        clientDO.setObjectVersionNumber(clientDTO.getObjectVersionNumber());
-        return ConvertHelper.convert(clientDO, ClientDTO.class);
+        //组织id不能修改
+        clientDTO.setOrganizationId(null);
+        return clientDTO;
     }
 }
