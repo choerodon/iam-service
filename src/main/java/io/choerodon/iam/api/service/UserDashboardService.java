ed2a64eb93c06859a72d184b3100ec79c19e6f1a
Fan <fan@choerodon.io>
2018-07-26 15:05:01 +0800

    [IMP] add userDashboard

--- src/main/java/io/choerodon/iam/api/service/UserDashboardService.java
+++ src/main/java/io/choerodon/iam/api/service/UserDashboardService.java
@@ -2,13 +2,14 @@ package io.choerodon.iam.api.service;
 
 import java.util.List;
 
+import io.choerodon.iam.api.dto.UserDashboardDTO;
 import io.choerodon.iam.domain.iam.entity.UserDashboard;
 
 /**
  * @author dongfan117@gmail.com
  */
 public interface UserDashboardService {
-    List<UserDashboard> list(String level, Long source_Id);
+    List<UserDashboardDTO> list(String level, Long source_Id);
 
-    List<UserDashboard> update(String level, Long source_Id, List<UserDashboard> dashboardList);
+    List<UserDashboardDTO> update(String level, Long source_Id, List<UserDashboard> dashboardList);
 }
