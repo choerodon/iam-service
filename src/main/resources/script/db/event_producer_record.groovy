4516f734558634094a11be3ea2c5724167f15597
superlee <superleader8@gmail.com>
2018-08-22 16:37:23 +0800

    [IMP] groovy 表名和字段名修改为大写

--- src/main/resources/script/db/event_producer_record.groovy
+++ src/main/resources/script/db/event_producer_record.groovy
@@ -3,16 +3,16 @@ package script.db
 databaseChangeLog(logicalFilePath: 'script/db/event_producer_record.groovy') {
     changeSet(id: '2018-05-18-add-table-event-producer-record', author: 'superleader8@gmail.com') {
         if(helper.dbType().isSupportSequence()){
-            createSequence(sequenceName: 'event_producer_record_s', startValue:"1")
+            createSequence(sequenceName: 'EVENT_PRODUCER_RECORD_S', startValue:"1")
         }
-        createTable(tableName: "event_producer_record") {
-            column(name: 'uuid', type: 'VARCHAR(50)', autoIncrement: false, remarks: 'uuid') {
+        createTable(tableName: "EVENT_PRODUCER_RECORD") {
+            column(name: 'UUID', type: 'VARCHAR(50)', autoIncrement: false, remarks: 'uuid') {
                 constraints(primaryKey: true)
             }
-            column(name: 'type', type: 'VARCHAR(50)', remarks: '业务类型') {
+            column(name: 'TYPE', type: 'VARCHAR(50)', remarks: '业务类型') {
                 constraints(nullable: false)
             }
-            column(name: 'create_time', type: 'DATETIME', remarks: '创建时间')
+            column(name: 'CREATE_TIME', type: 'DATETIME', remarks: '创建时间')
         }
     }
 }
