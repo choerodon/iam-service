25f93b02bed8d565d5c34afafb6b2f618744d014
dengyouquan <youquan.deng@hand-china.com>
2018-11-20 16:49:31 +0800

    [FIX] fix send notice;

--- src/main/java/io/choerodon/iam/domain/service/impl/IUserServiceImpl.java
+++ src/main/java/io/choerodon/iam/domain/service/impl/IUserServiceImpl.java
@@ -10,7 +10,6 @@ import io.choerodon.iam.infra.feign.NotifyFeignClient;
 import io.choerodon.mybatis.service.BaseServiceImpl;
 import org.springframework.stereotype.Service;
 
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -61,30 +60,9 @@ public class IUserServiceImpl extends BaseServiceImpl<UserDO> implements IUserSe
         NoticeSendDTO noticeSendDTO = new NoticeSendDTO();
         noticeSendDTO.setCode(code);
         noticeSendDTO.setSourceId(sourceId);
-        NoticeSendDTO.User currentUser = new NoticeSendDTO.User();
-        currentUser.setId(fromUserId);
-        UserE currentUserE = userRepository.selectByPrimaryKey(fromUserId);
-        if (currentUserE != null) {
-            currentUser.setEmail(currentUserE.getEmail());
-            currentUser.setLoginName(currentUserE.getLoginName());
-            currentUser.setRealName(currentUserE.getRealName());
-        }
-        noticeSendDTO.setFromUser(currentUser);
+        noticeSendDTO.setFromUserId(fromUserId);
+        noticeSendDTO.setTargetUsersIds(userIds);
         noticeSendDTO.setParams(params);
-        List<NoticeSendDTO.User> users = new LinkedList<>();
-        userIds.forEach(id -> {
-            NoticeSendDTO.User user = new NoticeSendDTO.User();
-            user.setId(id);
-            UserE userE = userRepository.selectByPrimaryKey(id);
-            if (userE != null) {
-                //有角色分配，但是角色已经删除
-                user.setEmail(userE.getEmail());
-                user.setLoginName(userE.getLoginName());
-                user.setRealName(userE.getRealName());
-                users.add(user);
-            }
-        });
-        noticeSendDTO.setTargetUsers(users);
         notifyFeignClient.postNotice(noticeSendDTO);
     }
 }
