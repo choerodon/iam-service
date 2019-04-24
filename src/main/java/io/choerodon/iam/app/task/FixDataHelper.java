4ca205172d673ea1e35c58052c55144909b46755
superlee <superleader8@gmail.com>
2019-04-24 17:50:14 +0800

    [FIX] 修复JobTask上加Transactional无法启动的问题

---
+++ src/main/java/io/choerodon/iam/app/task/FixDataHelper.java
@@ -0,0 +1,138 @@
+package io.choerodon.iam.app.task;
+
+import io.choerodon.base.enums.MenuType;
+import io.choerodon.base.enums.ResourceType;
+import io.choerodon.core.exception.CommonException;
+import io.choerodon.iam.infra.dto.MenuDTO;
+import io.choerodon.iam.infra.dto.MenuPermissionDTO;
+import io.choerodon.iam.infra.mapper.MenuMapper;
+import io.choerodon.iam.infra.mapper.MenuPermissionMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class FixDataHelper {
+
+    private final Logger logger = LoggerFactory.getLogger(FixDataHelper.class);
+
+    private static final String TOP_SITE = "choerodon.code.top.site";
+    private static final String TOP_ORGANIZATION = "choerodon.code.top.organization";
+    private static final String TOP_PROJECT = "choerodon.code.top.project";
+    private static final String TOP_USER = "choerodon.code.top.user";
+
+    private MenuPermissionMapper menuPermissionMapper;
+
+    private MenuMapper menuMapper;
+
+    public FixDataHelper(MenuPermissionMapper menuPermissionMapper, MenuMapper menuMapper) {
+        this.menuPermissionMapper = menuPermissionMapper;
+        this.menuMapper = menuMapper;
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public void fix(){
+        fixMenuPermissionData();
+        fixMenuData();
+    }
+
+    private void fixMenuData() {
+        logger.info("start to fix data in iam_menu");
+        Map<String, Long> topMenuMap = getTopMenu();
+
+        updateParentId(topMenuMap);
+
+        updateType("menu", MenuType.MENU_ITEM.value());
+        updateType("root", MenuType.MENU.value());
+        updateType("dir", MenuType.MENU.value());
+
+        updateResourceLevel();
+    }
+
+    private void updateResourceLevel() {
+        MenuDTO example = new MenuDTO();
+        example.setResourceLevel("user");
+        menuMapper.select(example).forEach(m -> {
+            m.setResourceLevel(ResourceType.SITE.value());
+            menuMapper.updateByPrimaryKey(m);
+        });
+    }
+
+    private void updateType(String oldType, String newType) {
+        MenuDTO example = new MenuDTO();
+        example.setType(oldType);
+        menuMapper.select(example).forEach(m -> {
+            m.setType(newType);
+            menuMapper.updateByPrimaryKey(m);
+        });
+    }
+
+    private void updateParentId(Map<String, Long> topMenuMap) {
+        for (Map.Entry<String, Long> entry : topMenuMap.entrySet()) {
+            String key = entry.getKey();
+            Long menuId = entry.getValue();
+            MenuDTO example = new MenuDTO();
+            example.setResourceLevel(key);
+            List<MenuDTO> menus = menuMapper.select(example);
+            menus.forEach(m -> {
+                if (m.getParentId().equals(0L)) {
+                    m.setParentId(menuId);
+                    menuMapper.updateByPrimaryKey(m);
+                }
+            });
+        }
+    }
+
+    private Map<String, Long> getTopMenu() {
+        Map<String, Long> map = new HashMap<>(4);
+        processMap(map, "site", TOP_SITE);
+        processMap(map, "organization", TOP_ORGANIZATION);
+        processMap(map, "project", TOP_PROJECT);
+        processMap(map, "user", TOP_USER);
+        return map;
+    }
+
+    private void processMap(Map<String, Long> map, String key, String code) {
+        MenuDTO example = new MenuDTO();
+        example.setCode(code);
+        MenuDTO menu = menuMapper.selectByPrimaryKey(example);
+        if (menu == null) {
+            throw new CommonException("can not find menu, code: " + code);
+        }
+        map.put(key, menu.getId());
+    }
+
+    /**
+     * 1.先删除menu_id已经不存在的脏数据
+     * 2.根据menu_id查menu,设置menu_code为对应menu的code
+     */
+    private void fixMenuPermissionData() {
+        logger.info("start to fix data in iam_menu_permission");
+        menuPermissionMapper.deleteDirtyData();
+        List<MenuPermissionDTO> menuPermissions = menuPermissionMapper.selectAll();
+        menuPermissions.forEach(
+                mp -> {
+                    String menuCode = mp.getMenuCode();
+                    Long menuId = null;
+                    try {
+                        menuId = Long.valueOf(menuCode);
+                    } catch (Exception e) {
+                        logger.error("cast code to id error, menuCode: {}", menuCode);
+                        return;
+                    }
+                    MenuDTO menu = menuMapper.selectByPrimaryKey(menuId);
+                    if (menu == null) {
+                        logger.error("can not find menu where id = {}", menuId);
+                        return;
+                    }
+                    mp.setMenuCode(menu.getCode());
+                    menuPermissionMapper.updateByPrimaryKey(mp);
+                }
+        );
+    }
+}
--- src/main/java/io/choerodon/iam/app/task/FixDataTask.java
+++ src/main/java/io/choerodon/iam/app/task/FixDataTask.java
@@ -4,16 +4,8 @@ import io.choerodon.asgard.schedule.QuartzDefinition;
 import io.choerodon.asgard.schedule.annotation.JobTask;
 import io.choerodon.asgard.schedule.annotation.TimedTask;
 import io.choerodon.core.iam.ResourceLevel;
-import io.choerodon.iam.infra.dto.MenuDTO;
-import io.choerodon.iam.infra.dto.MenuPermissionDTO;
-import io.choerodon.iam.infra.mapper.MenuMapper;
-import io.choerodon.iam.infra.mapper.MenuPermissionMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -25,57 +17,17 @@ import java.util.Map;
 @Component
 public class FixDataTask {
 
-    private final Logger logger = LoggerFactory.getLogger(FixDataTask.class);
+    private FixDataHelper fixDataHelper;
 
-    private MenuPermissionMapper menuPermissionMapper;
-
-    private MenuMapper menuMapper;
-
-    public FixDataTask(MenuPermissionMapper menuPermissionMapper, MenuMapper menuMapper) {
-        this.menuPermissionMapper = menuPermissionMapper;
-        this.menuMapper = menuMapper;
+    public FixDataTask(FixDataHelper fixDataHelper) {
+        this.fixDataHelper = fixDataHelper;
     }
 
     @TimedTask(name = "重构iam表结构后修复数据，只执行一次", description = "重构iam表结构后修复数据，只执行一次",
             oneExecution = true, params = {}, repeatCount = 0, repeatInterval = 0, repeatIntervalUnit = QuartzDefinition.SimpleRepeatIntervalUnit.SECONDS)
     @JobTask(maxRetryCount = 3, code = "fixData", level = ResourceLevel.SITE, description = "重构iam表结构后修复数据任务")
-    @Transactional(rollbackFor = Exception.class)
     public void fixData(Map<String, Object> map) {
-        fixMenuPermissionData();
-        fixMenuData();
+        fixDataHelper.fix();
     }
 
-    private void fixMenuData() {
-        logger.info("start to fix data in iam_menu");
-
-    }
-
-    /**
-     * 1.先删除menu_id已经不存在的脏数据
-     * 2.根据menu_id查menu,设置menu_code为对应menu的code
-     */
-    private void fixMenuPermissionData() {
-        logger.info("start to fix data in iam_menu_permission");
-        menuPermissionMapper.deleteDirtyData();
-        List<MenuPermissionDTO> menuPermissions = menuPermissionMapper.selectAll();
-        menuPermissions.forEach(
-                mp -> {
-                    String menuCode = mp.getMenuCode();
-                    Long menuId = null;
-                    try {
-                        menuId = Long.valueOf(menuCode);
-                    } catch (Exception e) {
-                        logger.error("cast code to id error, menuCode: {}", menuCode);
-                        return;
-                    }
-                    MenuDTO menu = menuMapper.selectByPrimaryKey(menuId);
-                    if (menu == null) {
-                        logger.error("can not find menu where id = {}", menuId);
-                        return;
-                    }
-                    mp.setMenuCode(menu.getCode());
-                    menuPermissionMapper.updateByPrimaryKey(mp);
-                }
-        );
-    }
 }
