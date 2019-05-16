a3b4727ef33d1e6dbc0a5fc1987e0aad31b78d0b
HuangQiii <598260403@qq.com>
2019-05-16 19:20:12 +0800

    [FIX] fix application manager, fix unused import

--- react/src/app/iam/containers/organization/application/index.js
+++ react/src/app/iam/containers/organization/application/index.js
@@ -5,11 +5,13 @@ import { asyncRouter, nomatch } from '@choerodon/boot';
 
 const index = asyncRouter(() => import('./Application'), () => import('../../../stores/organization/application'));
 const manageApplication = asyncRouter(() => import('./ManageApplication'), () => import('../../../stores/organization/application/AppManageStore'));
+const addApplication = asyncRouter(() => import('./AddApplication'), () => import('../../../stores/organization/application'));
 
 const Index = ({ match }) => (
   <Switch>
     <Route exact path={match.url} component={index} />
     <Route exact path={`${match.url}/manage/:applicationId`} component={manageApplication} />
+    <Route exact path={`${match.url}/add`} component={addApplication} />
     <Route path="*" component={nomatch} />
   </Switch>
 );
