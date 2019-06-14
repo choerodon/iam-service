8bca2525a7936eb2bebdb64b1128c3f6b9bca6c2
caojiameng <jiameng.cao@hand-china.com>
2019-06-14 16:04:54 +0800

    [ADD] 添加项目群子项目的projectMapCategory

--- src/main/java/io/choerodon/iam/app/service/impl/ProjectRelationshipServiceImpl.java
+++ src/main/java/io/choerodon/iam/app/service/impl/ProjectRelationshipServiceImpl.java
@@ -5,14 +5,18 @@ import io.choerodon.asgard.saga.producer.StartSagaBuilder;
 import io.choerodon.asgard.saga.producer.TransactionalProducer;
 import io.choerodon.core.exception.CommonException;
 import io.choerodon.core.iam.ResourceLevel;
+import io.choerodon.iam.api.dto.ProjectCategoryDTO;
 import io.choerodon.iam.api.dto.RelationshipCheckDTO;
 import io.choerodon.iam.api.dto.payload.ProjectRelationshipInsertPayload;
 import io.choerodon.iam.app.service.ProjectRelationshipService;
 import io.choerodon.iam.domain.repository.ProjectRelationshipRepository;
 import io.choerodon.iam.domain.repository.ProjectRepository;
 import io.choerodon.iam.infra.dto.ProjectDTO;
+import io.choerodon.iam.infra.dto.ProjectMapCategoryDTO;
 import io.choerodon.iam.infra.dto.ProjectRelationshipDTO;
 import io.choerodon.iam.infra.enums.ProjectCategory;
+import io.choerodon.iam.infra.mapper.ProjectCategoryMapper;
+import io.choerodon.iam.infra.mapper.ProjectMapCategoryMapper;
 import io.choerodon.iam.infra.mapper.ProjectRelationshipMapper;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
@@ -55,13 +59,18 @@ public class ProjectRelationshipServiceImpl implements ProjectRelationshipServic
     private ProjectRepository projectRepository;
     private TransactionalProducer producer;
     private ProjectRelationshipMapper relationshipMapper;
+    private ProjectCategoryMapper projectCategoryMapper;
+    private ProjectMapCategoryMapper projectMapCategoryMapper;
 
     public ProjectRelationshipServiceImpl(ProjectRelationshipRepository projectRelationshipRepository, ProjectRepository projectRepository,
-                                          TransactionalProducer producer, ProjectRelationshipMapper relationshipMapper) {
+                                          TransactionalProducer producer, ProjectRelationshipMapper relationshipMapper,
+                                          ProjectCategoryMapper projectCategoryMapper, ProjectMapCategoryMapper projectMapCategoryMapper) {
         this.projectRelationshipRepository = projectRelationshipRepository;
         this.projectRepository = projectRepository;
         this.producer = producer;
         this.relationshipMapper = relationshipMapper;
+        this.projectCategoryMapper = projectCategoryMapper;
+        this.projectMapCategoryMapper = projectMapCategoryMapper;
     }
 
     @Override
@@ -84,7 +93,15 @@ public class ProjectRelationshipServiceImpl implements ProjectRelationshipServic
         if (projectRelationshipDTO == null) {
             throw new CommonException(RELATIONSHIP_NOT_EXIST_EXCEPTION);
         }
-
+        ProjectMapCategoryDTO projectMapCategoryDTO = new ProjectMapCategoryDTO();
+        projectMapCategoryDTO.setProjectId(projectRelationshipDTO.getProjectId());
+        ProjectCategoryDTO projectCategoryDTO = new ProjectCategoryDTO();
+        projectCategoryDTO.setCode("PROGRAM");
+        projectCategoryDTO = projectCategoryMapper.selectOne(projectCategoryDTO);
+        projectMapCategoryDTO.setCategoryId(projectCategoryDTO.getId());
+        if (projectMapCategoryMapper.delete(projectMapCategoryDTO) != 1) {
+            throw new CommonException("error.projectMapCategory.delete");
+        }
         ProjectRelationshipInsertPayload sagaPayload = new ProjectRelationshipInsertPayload();
         ProjectDTO parent = projectRepository.selectByPrimaryKey(projectRelationshipDTO.getParentId());
         sagaPayload.setCategory(parent.getCategory());
@@ -197,6 +214,15 @@ public class ProjectRelationshipServiceImpl implements ProjectRelationshipServic
             BeanUtils.copyProperties(projectRelationshipRepository.addProjToGroup(relationshipDTO), relationshipDTO);
             returnList.add(relationshipDTO);
             // fill the saga payload
+            ProjectCategoryDTO projectCategoryDTO = new ProjectCategoryDTO();
+            projectCategoryDTO.setCode("PROGRAM");
+            projectCategoryDTO = projectCategoryMapper.selectOne(projectCategoryDTO);
+            ProjectMapCategoryDTO projectMapCategoryDTO = new ProjectMapCategoryDTO();
+            projectMapCategoryDTO.setProjectId(relationshipDTO.getProjectId());
+            projectMapCategoryDTO.setCategoryId(projectCategoryDTO.getId());
+            if (projectMapCategoryMapper.insert(projectMapCategoryDTO) != 1) {
+                throw new CommonException("error.projectMapCategory.insert");
+            }
             ProjectDTO project = projectRepository.selectByPrimaryKey(relationshipDTO.getProjectId());
             ProjectRelationshipInsertPayload.ProjectRelationship relationship
                     = new ProjectRelationshipInsertPayload.ProjectRelationship(project.getId(), project.getCode(),
---
+++ src/main/java/io/choerodon/iam/infra/mapper/ProjectCategoryMapper.java
@@ -0,0 +1,16 @@
+package io.choerodon.iam.infra.mapper;
+
+
+import io.choerodon.iam.api.dto.ProjectCategoryDTO;
+import io.choerodon.mybatis.common.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * @author jiameng.cao
+ * @date 2019/6/4
+ */
+public interface ProjectCategoryMapper extends Mapper<ProjectCategoryDTO> {
+
+}
